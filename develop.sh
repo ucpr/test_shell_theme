6d735add3c1589425de4fcbf456624285287b946
ucpr <hoge.uchihara@gmail.com>
2019-10-01 23:27:34 +0900

    feat: setting default value when environment variable is empty (#13)

--- develop.sh
+++ develop.sh
@@ -7,14 +7,6 @@
 # $ source develop.sh
 #
 
-export HINA_GIT_MODIFIED="*"
-export HINA_GIT_ADDED="+"
-export HINA_GIT_DELETED="-"
-export HINA_GIT_RENAMED="$"
-export HINA_GIT_COPIED="~"
-export HINA_GIT_UNMERGED="="
-export HINA_GIT_UNTRACKED="!"
-
 export HINA_PATH="/home/ucpr/Works/hina/hina"
 
 autoload -Uz add-zsh-hook
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ucpr/hina
 
 go 1.13
 
-require github.com/logrusorgru/aurora v0.0.0-20190803045625-94edacc10f9b
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/logrusorgru/aurora v0.0.0-20190803045625-94edacc10f9b
+)
